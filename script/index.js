5c4ad42800c1f5a2ddf3cfb8b59adbc2fb49bf07
Alexander Gninenko <alexander.gninenko@icloud.com>
2022-03-17 20:25:33 +1000

    review

--- script/index.js
+++ script/index.js
@@ -1,41 +1,41 @@
 
 
-const edit_btn = document.querySelector('.profile__edit-btn');
+const editBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
-const close_btn = document.querySelector('.popup__close-icon');
-const popup_name = document.querySelector('.popup__name');
-const popup_status = document.querySelector('.popup__status');
-const profile_name = document.querySelector('.profile__name');
-const profile_status = document.querySelector('.profile__status');
+const closeBtn = document.querySelector('.popup__close-icon');
+const popupName = document.querySelector('.popup__profile-name');
+const popupStatus = document.querySelector('.popup__profile-status');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
 const form = document.querySelector('.popup__form');
-const like_btn = document.querySelectorAll('.photo-grid__like-btn');
+// const likeBtn = document.querySelectorAll('.photo-grid__like-btn');
 
 
 
-const show =()=> {
-    popup_name.value = profile_name.textContent;
-    popup_status.value = profile_status.textContent;
+const showProfilePopup =()=> {
+    popupName.value = profileName.textContent;
+    popupStatus.value = profileStatus.textContent;
     popup.classList.add('popup_opened');
 }
 
-const hide =()=> {
+const hideProfilePopup =()=> {
     popup.classList.remove('popup_opened');
 }
 
-const save = (e) => {
+const saveProfileInfo = (e) => {
     e.preventDefault();
-    profile_name.textContent = popup_name.value;
-    profile_status.textContent = popup_status.value;
-    hide();
+    profileName.textContent = popupName.value;
+    profileStatus.textContent = popupStatus.value;
+    hideProfilePopup();
 }
 
-const isLiked = (e) => {
-    e.target.classList.toggle('photo-grid__like-btn_active');
-}
+// const isLiked = (e) => {
+//     e.target.classList.toggle('photo-grid__like-btn_active');
+// }
 
-like_btn.forEach(like => like.addEventListener('click', isLiked));
+// likeBtn.forEach(like => like.addEventListener('click', isLiked));
 
 
-edit_btn.addEventListener('click', show);
-close_btn.addEventListener('click', hide);
-form.addEventListener('submit', save);
+editBtn.addEventListener('click', showProfilePopup);
+closeBtn.addEventListener('click', hideProfilePopup);
+form.addEventListener('submit', saveProfileInfo);
