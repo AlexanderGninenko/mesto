eeb97a843a1463cf0a80c5a1cf3da09e99f2a632
Alexander Gninenko <alexander.gninenko@icloud.com>
2022-03-28 18:51:10 +1000

    added cards from array in js

--- script/index.js
+++ script/index.js
@@ -1,5 +1,32 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
 
+const cardsContainer = document.querySelector('.photo-grid__wrapper');
 const editBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const closeBtn = document.querySelector('.popup__close-icon');
@@ -8,8 +35,26 @@ const popupStatus = document.querySelector('.popup__input_profile_status');
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const form = document.querySelector('.popup__form');
-// const likeBtn = document.querySelectorAll('.photo-grid__like-btn');
 
+const createCard = (card) => {
+    const template = document.querySelector('#card-template').content;
+    const cardItem = template.querySelector('.photo-grid__item').cloneNode(true);
+    let photoImageLink = cardItem.querySelector('.photo-grid__image');
+    let photoImageAltName = cardItem.querySelector('.photo-grid__image');
+    let cardItemTitle = cardItem.querySelector('.photo-grid__title');
+    const likeBtn = cardItem.querySelector('.photo-grid__like-btn');
+    likeBtn.addEventListener('click', isLiked);
+    cardItemTitle.textContent = card.name;
+    photoImageLink.src = card.link;
+    photoImageAltName.alt = card.name;
+
+    return cardItem;
+}
+
+const renderCard = (card) => {
+    cardsContainer.prepend(card);
+
+}
 
 
 const showProfilePopup =()=> {
@@ -29,13 +74,13 @@ const saveProfileInfo = (e) => {
     hideProfilePopup();
 }
 
-// const isLiked = (e) => {
-//     e.target.classList.toggle('photo-grid__like-btn_active');
-// }
-
-// likeBtn.forEach(like => like.addEventListener('click', isLiked));
+const isLiked = (e) => {
+    e.target.classList.toggle('photo-grid__like-btn_active');
+}
 
 
 editBtn.addEventListener('click', showProfilePopup);
 closeBtn.addEventListener('click', hideProfilePopup);
 form.addEventListener('submit', saveProfileInfo);
+
+initialCards.forEach(card => renderCard(createCard(card)));
